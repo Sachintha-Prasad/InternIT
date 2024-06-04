ea6f3a9ad14d23b6478bcc7d984013fead18effb
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-04 16:48:16 +0530

    initialize routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react"
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider
+} from "react-router-dom"
+
+import MainLayout from "./layouts/MainLayout"
 
 const App = () => {
-    return <div></div>
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<MainLayout />}></Route>
+        )
+    )
+
+    return <RouterProvider router={router} />
 }
 
 export default App
