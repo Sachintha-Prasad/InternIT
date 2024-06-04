4393def03d8b4c2fc7bdb5e121f62557ea3953e7
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-04 22:59:48 +0530

    Not found page done

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,7 +1,25 @@
 import React from "react"
+import notFoundImg from "../assets/not-found-img.svg"
+import { Link } from "react-router-dom"
 
 const NotFoundPage = () => {
-    return <div>NotFoundPage</div>
+    return (
+        <div className="container flex gap-8 flex-col py-12 items-center">
+            <div className="w-full max-w-[400px]">
+                <img src={notFoundImg} alt="" />
+            </div>
+            <h1 className="text-2xl lg:text-6xl font-bold">
+                Page <span className="text-dark-blue">not Found</span>
+            </h1>
+            <p className="text-lg font-medium">
+                This page is currently not available or still developing
+            </p>
+
+            <Link to={"/"} className="cta-btn">
+                Return to Homepage
+            </Link>
+        </div>
+    )
 }
 
 export default NotFoundPage
