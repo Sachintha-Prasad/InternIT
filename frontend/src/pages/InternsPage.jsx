f16e31cbe0034d514b63eb3c7dc5afd2fce03c39
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-04 22:35:45 +0530

    all interns page done

---
+++ frontend/src/pages/InternsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import InternList from "../components/InternList"
+
+const InternsPage = () => {
+    return (
+        <>
+            <InternList />
+        </>
+    )
+}
+
+export default InternsPage
