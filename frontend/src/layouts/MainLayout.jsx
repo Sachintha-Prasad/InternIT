522ac994fc48f4d4d02c984e48e9b7178d9b2167
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-04 22:36:22 +0530

    main layout

---
+++ frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/ReactToastify.css"
+import Navbar from "../components/Navbar"
+import { Outlet } from "react-router-dom"
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar />
+            <Outlet />
+            <ToastContainer />
+        </div>
+    )
+}
+
+export default MainLayout
