2c84997435a905e687985851462493c15399248a
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-04 22:19:19 +0530

    spinner component for loading animation

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,19 @@
+import ClipLoader from "react-spinners/ClipLoader"
+
+const override = {
+    display: "block",
+    margin: "0 auto"
+}
+
+function Spinner({ loading }) {
+    return (
+        <ClipLoader
+            color="#0077B6"
+            loading={loading}
+            cssOverride={override}
+            size={150}
+        />
+    )
+}
+
+export default Spinner
