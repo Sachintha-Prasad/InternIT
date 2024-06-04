1469ad794c8d5ca1f7dab0ddb5fd711662ce596b
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-04 21:39:31 +0530

    intern list component

---
+++ frontend/src/components/IntenList.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react"
+import InternCard from "./InternCard"
+
+const IntenList = () => {
+    const [internList, setInternList] = useState([])
+
+    useEffect(() => {
+        try {
+            const fetchInterns = async () => {
+                const res = await fetch("http://localhost:5000/internships")
+                const data = await res.json()
+                setInternList(data)
+            }
+
+            fetchInterns()
+        } catch (error) {
+            console.log("Data fetching error", error)
+        }
+    }, [])
+
+    return (
+        <div className="bg-blue-50">
+            <div className="container py-12 flex flex-col gap-6">
+                <div className="flex justify-center">
+                    <h2 className="text-2xl lg:text-3xl text-dark-gray font-semibold">
+                        Recent{" "}
+                        <span className="text-dark-blue">Internships</span>
+                    </h2>
+                </div>
+                <div className="grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+                    {internList.map((intern) => (
+                        <InternCard key={intern.id} intern={intern} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default IntenList
