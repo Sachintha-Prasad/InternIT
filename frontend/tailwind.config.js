97454749dbc581b38af407ee23a273dce2597b63
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-05 13:55:26 +0530

    style changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,15 +3,6 @@ export default {
     content: ["./index.html", "./src/**/*.{js,jsx}"],
     theme: {
         extend: {
-            colors: {
-                "dark-blue": "#0077B6",
-                "light-blue": "#C2F0FF",
-                "dark-gray": "#343A40",
-                "light-gray": "#888888",
-                white: "#FFFFFF",
-                cream: "#FFFDEF"
-            },
-
             container: {
                 center: true,
                 padding: "2rem"
