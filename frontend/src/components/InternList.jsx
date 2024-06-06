6616a15d358af51cb5f99f4e18110ad87d7c085c
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-06 15:21:57 +0530

    implement the search functionality

--- frontend/src/components/InternList.jsx
+++ frontend/src/components/InternList.jsx
@@ -1,58 +1,105 @@
-import React, { useEffect, useState } from "react"
-import InternCard from "./InternCard"
-import Spinner from "../components/Spinner"
-import { CiGrid2H, CiGrid41 } from "react-icons/ci"
-import Pagination from "./Pagination"
+import React, { useEffect, useState } from 'react'
+import InternCard from './InternCard'
+import Spinner from '../components/Spinner'
+import { CiGrid2H, CiGrid41, CiSearch } from 'react-icons/ci'
+import Pagination from './Pagination'
 
 const InternList = () => {
     const [internList, setInternList] = useState([])
     const [loading, setLoading] = useState(true)
-
     const [isGrid, setIsGrid] = useState(true)
+    const [searchQuery, setSearchQuery] = useState('')
+    const [filteredInternList, setFilteredInternList] = useState([])
 
+    // handle search
+    const handleSearch = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        try {
+            const res = await fetch('http://localhost:5000/internships')
+            const data = await res.json()
+            const filteredData = data.filter((intern) =>
+                intern.title?.toLowerCase().includes(searchQuery.toLowerCase())
+            )
+            setFilteredInternList(filteredData)
+        } catch (error) {
+            console.log('Data fetching error', error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    // get all the data
+    useEffect(() => {
+        const fetchInterns = async () => {
+            try {
+                const res = await fetch('http://localhost:5000/internships')
+                const data = await res.json()
+                setInternList(data)
+                setFilteredInternList(data)
+            } catch (error) {
+                console.log('Data fetching error', error)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchInterns()
+    }, [])
+
+    // handle list view
     const handleListView = () => {
         setIsGrid((prevState) => !prevState)
     }
 
-    // pagination
+    // Pagination
     const [currentPage, setCurrentPage] = useState(1)
     const [cardsPerPage, setCardsPerPage] = useState(6)
 
     const lastCardIndex = currentPage * cardsPerPage
     const firstCardIndex = lastCardIndex - cardsPerPage
 
-    const newInternList = internList.slice(firstCardIndex, lastCardIndex)
-
-    // pagination
-
-    useEffect(() => {
-        try {
-            const fetchInterns = async () => {
-                const res = await fetch("http://localhost:5000/internships")
-                const data = await res.json()
-                setInternList(data)
-            }
-
-            fetchInterns()
-        } catch (error) {
-            console.log("Data fetching error", error)
-        } finally {
-            setLoading(false)
-        }
-    }, [])
+    const paginatedInternList = filteredInternList.slice(
+        firstCardIndex,
+        lastCardIndex
+    )
 
     return (
         <div>
-            <div className="container py-6 flex flex-col gap-6">
-                <div
-                    className="self-end p-3 hover:bg-indigo-50 rounded-full cursor-pointer"
-                    onClick={handleListView}
-                >
-                    {isGrid ? (
-                        <CiGrid2H className="text-indigo-500 text-3xl " />
-                    ) : (
-                        <CiGrid41 className="text-indigo-500 text-3xl " />
-                    )}
+            <div className="container flex flex-col gap-6 py-6">
+                <div className="mb-6 flex w-full justify-between gap-3">
+                    <form
+                        className="flex w-full max-w-lg items-center"
+                        onSubmit={handleSearch}
+                    >
+                        <div className="w-full">
+                            <input
+                                type="text"
+                                id="simple-search"
+                                className="sda-s block size-[42px] w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-800 focus:border-blue-500 focus:outline-indigo-500 focus:ring-blue-500"
+                                placeholder="Search intern name..."
+                                value={searchQuery}
+                                onChange={(e) => setSearchQuery(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="group ms-2 flex size-[42px] items-center justify-center rounded-lg border bg-indigo-500 text-sm font-medium text-white hover:bg-indigo-50"
+                        >
+                            <CiSearch className="text-xl group-hover:text-gray-800" />
+                        </button>
+                    </form>
+                    <div
+                        className="cursor-pointer rounded-full p-3 hover:bg-indigo-50"
+                        onClick={handleListView}
+                    >
+                        {isGrid ? (
+                            <CiGrid2H className="text-3xl text-indigo-500" />
+                        ) : (
+                            <CiGrid41 className="text-3xl text-indigo-500" />
+                        )}
+                    </div>
                 </div>
 
                 {loading ? (
@@ -61,10 +108,10 @@ const InternList = () => {
                     <div
                         className={`${
                             isGrid &&
-                            "grid-cols-1 md:grid-cols-2 xl:grid-cols-3"
+                            'grid-cols-1 md:grid-cols-2 xl:grid-cols-3'
                         } grid grid-cols-1 gap-8`}
                     >
-                        {newInternList.map((intern) => (
+                        {paginatedInternList.map((intern) => (
                             <InternCard
                                 key={intern.id}
                                 intern={intern}
@@ -75,7 +122,7 @@ const InternList = () => {
                 )}
 
                 <Pagination
-                    totalCards={internList.length}
+                    totalCards={filteredInternList.length}
                     cardsPerPage={cardsPerPage}
                     currentPage={currentPage}
                     setCurrentPage={setCurrentPage}
