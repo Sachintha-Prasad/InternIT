315bf091c14dbc5ba08ce70e6c190a7d44dbfb41
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-07-24 00:31:06 +0530

    internship repository

---
+++ backend/src/main/java/com/InternIT/backend/repository/InternshipRepository.java
@@ -0,0 +1,9 @@
+package com.internit.backend.repository;
+
+import com.internit.backend.entity.Internship;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InternshipRepository extends JpaRepository<Internship, Long> {
+}
