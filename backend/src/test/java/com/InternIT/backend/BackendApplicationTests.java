98c8425c3db0423b295d68fc6f70cda0ef4b0fcd
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-07-23 21:06:40 +0530

    backend intial commit

---
+++ backend/src/test/java/com/InternIT/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InternIT.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
